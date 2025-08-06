b7195bdb24cf0b1cfb2444ad696bcc26908b3c97
yrahate7 <yrahate7@gmail.com>
2025-08-06 10:52:15 +0530

    Implement Infinite Loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,20 +4,22 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let secret_number = rand::rng().random_range(1..=100);
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    // By default in rust, you cannot redeclare variables, However, if needed, you can shadow them
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {guess}");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
+    loop {
+        println!("Please input your guess.");
+        let secret_number = rand::rng().random_range(1..=100);
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        // By default in rust, you cannot redeclare variables, However, if needed, you can shadow them
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
+    }
 }
