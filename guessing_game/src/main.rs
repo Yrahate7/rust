2031542aef52a4330ec4376728c04903f6f760b6
yrahate7 <yrahate7@gmail.com>
2025-08-06 10:48:57 +0530

    Shadowing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,9 +10,10 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
-    println!("Secret number is {}", secret_number);
-    match guess.cmp(&secret_number.to_string()) {
+    // By default in rust, you cannot redeclare variables, However, if needed, you can shadow them
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("You guessed: {guess}");
+    match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal => println!("You win!"),
